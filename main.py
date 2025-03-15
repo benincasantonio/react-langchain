ef778562eccb11560192e4770c46d73e4a8fc644
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-03-15 07:50:06 +0100

    feat: add tool lookup function and execute it

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from typing import Union
+from typing import Union, List
 from dotenv import load_dotenv
 from langchain_core.agents import AgentAction, AgentFinish
-from langchain.agents import tool
+from langchain.agents import tool, Tool
 from langchain.prompts import PromptTemplate
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_ollama import ChatOllama
+
 load_dotenv()
 
 
@@ -15,10 +16,18 @@ def get_text_length(text: str) -> int:
     return len(text)
 
 
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+
+    raise f"The tool with name {tool_name} was not found"
+
+
 if __name__ == "__main__":
     print("Hello ReAct Langchain!")
 
-    tools = [get_text_length]
+    tools: List[Tool] = [get_text_length]
 
     template = """
     Answer the following questions as best you can. You have access to the following tools:
@@ -54,6 +63,17 @@ if __name__ == "__main__":
 
     chain = {"input": lambda x:x["input"]} | prompt_template | llm | ReActSingleInputOutputParser()
 
-    agent_step: Union[AgentAction, AgentFinish] = chain.invoke({"input": "What is the length of 'GTA V'?"})
+    agent_step: Union[AgentAction, AgentFinish] = chain.invoke({"input": "What is the length of 'Capibara'?"})
 
-    print(agent_step)
+    if isinstance(agent_step, AgentAction):
+
+        tool_name = agent_step.tool
+        tool_to_run = find_tool_by_name(tools, tool_name)
+
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_run.func(str(tool_input))
+        print(f"{observation=}")
+
+
+
